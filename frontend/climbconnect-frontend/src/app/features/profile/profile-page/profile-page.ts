6758da757308dfa8758f9a90d0d3d3773e482324
Mo.Za <attiamo112005@gmail.com>
2025-12-16 15:56:52 +0100

    Angular project + routing + basic pages

---
+++ frontend/climbconnect-frontend/src/app/features/profile/profile-page/profile-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-page.html',
+  styleUrl: './profile-page.scss',
+})
+export class ProfilePage {
+
+}
