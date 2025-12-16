1a75502219632ef9db627a80f48f4e0c4fcdd1fd
Mo.Za <attiamo112005@gmail.com>
2025-12-16 16:23:39 +0100

    area detail page with route param

---
+++ frontend/climbconnect-frontend/src/app/features/areas/area-detail-page/area-detail-page.ts
@@ -0,0 +1,16 @@
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-area-detail-page',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './area-detail-page.html',
+  styleUrl: './area-detail-page.scss',
+})
+export class AreaDetailPage {
+  private route = inject(ActivatedRoute);
+
+  id = this.route.snapshot.paramMap.get('id');
+}
+
--- frontend/climbconnect-frontend/src/app/features/areas/areas-page/areas-page.ts
+++ frontend/climbconnect-frontend/src/app/features/areas/areas-page/areas-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-areas-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './areas-page.html',
   styleUrl: './areas-page.scss',
 })
