c7f02af2c67fb0e841f1dee6c90ae141b63a757a
Mo.Za <attiamo112005@gmail.com>
2025-12-17 08:54:09 +0100

    API-Anbindung getestet

--- frontend/climbconnect-frontend/src/app/features/areas/areas-page/areas-page.ts
+++ frontend/climbconnect-frontend/src/app/features/areas/areas-page/areas-page.ts
@@ -1,21 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AreasApiService } from '../areas-api';
 
+import { AreasApiService, Area } from '../areas-api';
 
 @Component({
   selector: 'app-areas-page',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './areas-page.html',
+  styleUrl: './areas-page.scss',
 })
-export class AreasPage {
-  areas: any[] = [];
+export class AreasPage implements OnInit {
+  areas: Area[] = [];
+  loading = false;
+  error: string | null = null;
 
-  constructor(private api: AreasApiService) {
-    this.api.getAreas().subscribe((data: any[]) => {
-      this.areas = data.slice(0, 5);
-      console.log('API areas:', this.areas);
+  constructor(private api: AreasApiService) {}
+
+  ngOnInit(): void {
+    this.loadAreas();
+  }
+
+  loadAreas(): void {
+    this.loading = true;
+    this.error = null;
+
+    this.api.getAreas().subscribe({
+      next: (data) => {
+        this.areas = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'API Fehler (siehe Console/Network)';
+        console.error(err);
+        this.loading = false;
+      },
     });
   }
 }
