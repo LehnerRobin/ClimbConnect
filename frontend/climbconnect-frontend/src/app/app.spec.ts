40fef6e653b6509fc4d7e9badaab13a8dedc5921
Mo.Za <attiamo112005@gmail.com>
2026-01-04 17:10:35 +0100

    Update app.spec.ts

--- frontend/climbconnect-frontend/src/app/app.spec.ts
+++ frontend/climbconnect-frontend/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, climbconnect-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, climbconnect-fron tend');
   });
 });
