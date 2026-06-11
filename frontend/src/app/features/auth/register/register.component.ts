cc13ad2d5c1525a476c4b19c27e9bfe6d30b87e7
Hamid <you@example.com>
2026-06-11 18:35:31 +0200

    register erfolgt

--- frontend/src/app/features/auth/register/register.component.ts
+++ frontend/src/app/features/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../../../../services/auth.service';
 
 @Component({
@@ -9,13 +10,34 @@ import { AuthService } from '../../../../services/auth.service';
 })
 export class RegisterComponent {
 
-  constructor(private authService: AuthService) {}
+  errorMessage = '';
+
+constructor(
+  private authService: AuthService,
+  private router: Router
+) {}
 
   register(
     username: string,
     email: string,
-    password: string
+    password: string,
+    confirmPassword: string
   ): void {
+      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailPattern.test(email)) {
+    this.errorMessage = 'Bitte eine gültige E-Mail-Adresse eingeben';
+    return;
+  }
+    if (password.length < 8) {
+    this.errorMessage = 'Passwort muss mindestens 8 Zeichen haben';
+    return;
+  }
+
+    if (password !== confirmPassword) {
+      this.errorMessage = 'Passwörter stimmen nicht überein';
+      return;
+    }
 
     const user = {
       username,
@@ -23,13 +45,36 @@ export class RegisterComponent {
       password
     };
 
-    this.authService.register(user).subscribe({
-      next: (response: any) => {
-        console.log('Registrierung erfolgreich', response);
-      },
-      error: (error: any) => {
-        console.error('Registrierung fehlgeschlagen', error);
-      }
-    });
+   this.authService.register(user).subscribe({
+  next: (response: any) => {
+
+  localStorage.setItem('token', response.token);
+
+  console.log('Registrierung erfolgreich', response);
+
+  this.router.navigate(['/home']);
+
+},
+  error: (error: any) => {
+
+  if (error.status === 409) {
+
+    if (error.error?.message?.includes('email')) {
+      this.errorMessage = 'E-Mail bereits vergeben';
+    }
+    else if (error.error?.message?.includes('username')) {
+      this.errorMessage = 'Username bereits vergeben';
+    }
+    else {
+      this.errorMessage = 'Benutzer existiert bereits';
+    }
+
+  } else {
+    this.errorMessage = 'Registrierung fehlgeschlagen';
+  }
+
+  console.error(error);
+}
+});
   }
 }
